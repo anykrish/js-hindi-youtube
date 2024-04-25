f121ffac525e266edd1bcdd0672b21753f499a69
anykrish <129771832+anykrish@users.noreply.github.com>
2024-04-25 15:44:30 +0000

    Day 1 tarik 25 March

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,8 +1,8 @@
-"use strict"; // treat all JS code as newer version
+"use strict"; // treat all JS code as newer version             by defualt aab yahi hota hai
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
+console.log(3           // is comment ke baad bhi ye chalega
     +
      3) // code readability should be high
 
@@ -18,12 +18,13 @@ let state;
 // bigint
 // string => ""
 // boolean => true/false
-// null => standalone value
+// null => standalone value             temp ki example
 // undefined => 
-// symbol => unique
+// symbol => unique                     react me dekhnge
 
 
 // object
 
+console.log(typeof "deadpool"); // string
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
